fe9aa6801c958051d17e7b806c09d7beb1c5ff41
linfeng521 <1471067206@qq.com>
2024-01-24 23:11:35 +0800

    完成项目初次搭建完成

---
+++ vueaction/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <hr />
+    <h2>{{ name }}</h2>
+    <el-button @click="showMsg">点击</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      name: "linfeng  github action 学习"
+    }
+  },
+  methods: {
+    showMsg() {
+      this.name = "linfeng521";
+      this.$message({
+        message: `恭喜${this.name}，这是一条成功消息`,
+        type: 'success'
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
